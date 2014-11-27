d6cf79896c418d4a7876b7672a5567e07657cc8e
roxma <roxma@qq.com>
2014-11-28 02:08:23 +0800

    usage optimization

--- easymake.mk
+++ easymake.mk
@@ -9,7 +9,6 @@
 
 # basic settings
 
-SETTINGS_ROOT?=build_settings
 # do not use ./bin
 BUILD_ROOT?=bin
 # TARGET=
@@ -20,6 +19,7 @@ CEXT?=c
 GCC?=gcc
 GXX?=g++
 LINKER?=g++
+AR?=ar
 
 ################################################################
 
@@ -73,11 +73,11 @@ GetEntryPath4Timestamp=$(if $(call SearchFilePath,$(1)),$(call SearchFilePath,$(
 
 ##
 # If the user specifies $(ENTRY), and the $(ENTRY) is not a file, update its 
-# timestamp
+# timestamp, so that this entry will be picked next time.
 ifneq ($(ENTRY),)
 ifeq ($(strip $(call SearchFilePath,$(ENTRY))),)
     $(shell mkdir -p $(dir $(BUILD_ROOT)/easy_make_entry_timestamp_$(ENTRY)))
-    $(shell  touch $(BUILD_ROOT)/easy_make_entry_timestamp_$(ENTRY))
+    $(shell touch $(BUILD_ROOT)/easy_make_entry_timestamp_$(ENTRY))
 endif
 endif
 
@@ -89,7 +89,7 @@ GetNewestFileIndex=$(shell newestIndex=1 && index=1 && newest=$(call GetEntryPat
 
 ##
 # A function to decide the actual entry file.
-# @param 1 entry
+# @param 1 the user-specified entry, could be an empty string
 # @param 2 entry_list
 # @param 3 error message if this function failed. If the entry is neither
 # empty nor in the entry_list.
@@ -100,60 +100,35 @@ GetEntry=$(if $(1),$(if $(call WordExist,$(1),$(2)),$(1),$(call SelectFirstMatch
 # @param 1 SOURCES 
 # @param 2 ENTRY 
 # @param 3 ENTRY_LIST
-SourcesToLink=$(filter-out $(filter-out $(2),$(3)), $(1))
+FilterSourcesToLink=$(filter-out $(filter-out $(2),$(3)), $(1))
 
 ##
 # @param 1 sources
 # @param 2 build_root
 # @param 3 source file extension
-CorrendingObjects=$(foreach _src,$(1),$(2)/$(_src:.$(3)=.o))
+GetCorrendingObjects=$(foreach _src,$(1),$(2)/$(_src:.$(3)=.o))
 
 ##
 # Recursive wildcard
 RWildcard=$(foreach d,$(wildcard $1*),$(call RWildcard,$d/,$2) $(filter $(subst *,%,$2),$d))
 
+##
+# @param 1 file name
+# @param 2 key
+ConfReadValue=$(shell cat $1 | awk  '{if ($$1=="$2") print $$2; }')
 
-# A list of directories where compiler search for headers.
-COMPILE_SEARCH_PATH += $(call ReadSettings,$(SETTINGS_ROOT)/compile_search_path) . $(VPATH)
-
-COMPILE_FLAGS += $(call ReadSettings, $(SETTINGS_ROOT)/compile_flags)
-
-LINK_SEARCH_PATH += $(call ReadSettings, $(SETTINGS_ROOT)/link_search_path)
-
-LINK_FLAGS += $(call ReadSettings, $(SETTINGS_ROOT)/link_flags)
-
-easy_make_f_info:=$(BUILD_ROOT)/easy_make_current_entry_file
-
-prev_entry:=$(call ReadLine,$(easy_make_f_info),1)
-prev_target:=$(call ReadLine,$(easy_make_f_info),2)
-# If $(ENTRY) is set by user, store it to file $(easy_make_f_info).
-# Otherwise, use the previous entry by default.
-ifneq (,$(ENTRY))
-    $(shell mkdir -p $(dir $(easy_make_f_info)))
-    $(shell echo "$(ENTRY)" > $(easy_make_f_info))
-    $(shell echo "$(prev_target)" >> $(easy_make_f_info))
-else
-#    ENTRY:=$(prev_entry)
-endif
-
-
-# For unit testing, a list of source files that contains the main function.
-# by default, only the first file will be in the $(SOURCES) list, others will 
-# be filter out
-ENTRY_LIST += $(call ReadSettings,$(SETTINGS_ROOT)/entry_list)
-
-# read the previous settings for 'TARGET', if empty, set to 'target'
-TARGET?=$(call ReadLine,$(easy_make_f_info),2)
-ifeq (,$(strip $(TARGET)))
-    TARGET:=target
-endif
+##
+# @param 1 file name
+# @param 2 key
+# @param 3 value
+CmdConfWriteValue= touch $1 ; fileContents="`cat $1`" ; echo "$$fileContents" |  awk '{if ((NF==2)&&($$1!="$2")) print $$1,$$2 ; } END{print "$2","$3" ;}' > $1
 
 
 ################################################################
 
 ifneq (,$(call BeginWith,./,$(BUILD_ROOT)))
     # "./" in BUILD_ROOT may cause entry detecting problem.
-    $(error Please don't use prefix "./" in variable BUILD_ROOT=$(BUILD_ROOT))
+    $(error Please do not use prefix "./" in variable BUILD_ROOT=$(BUILD_ROOT))
 endif
 
 # if CPPSOURCES are not specified, automatically scan all .$(CPPEXT) files in the 
@@ -165,8 +140,8 @@ endif
 ifneq (,$(findstring ..,$(CPPSOURCES)))
     $(error ".." should not appear in the cpp source list: $(CPPSOURCES))
 endif
-# delete "./" in file path, which may cause dependency problems.
-CPPSOURCES:=$(subst ./,,$(CPPSOURCES)) 
+# remove "./" in file path, which may cause pattern rules problems.
+CPPSOURCES:=$(subst ./,,$(CPPSOURCES))
 
 # if CSOURCES are not specified, automatically scan all .$(CEXT) files in the 
 # current directories.
@@ -177,40 +152,31 @@ endif
 ifneq (,$(findstring ..,$(CSOURCES)))
     $(error ".." should not appear in the c source list: $(CSOURCES))
 endif
-# delete "./" in file path, which may cause dependency problems.
+# remove "./" in file path, which may cause pattern rules problems.
 CSOURCES:=$(subst ./,,$(CSOURCES))
 
 
-easy_make_all_cppobjects:=$(call CorrendingObjects,$(CPPSOURCES),$(BUILD_ROOT),$(CPPEXT))
-easy_make_all_cobjects:=$(call CorrendingObjects,$(CSOURCES),$(BUILD_ROOT),$(CEXT))
-easy_make_target:=$(BUILD_ROOT)/$(TARGET)
-# $(easy_make_f_detected_entries) - A file that contains a list of entries detected by easy_make.
+easy_make_all_cppobjects:=$(call GetCorrendingObjects,$(CPPSOURCES),$(BUILD_ROOT),$(CPPEXT))
+easy_make_all_cobjects:=$(call GetCorrendingObjects,$(CSOURCES),$(BUILD_ROOT),$(CEXT))
+
+
+# A file that contains a list of entries detected by easy_make.
 easy_make_f_detected_entries:=$(BUILD_ROOT)/easy_make_detected_entries
 
-# The file $(easy_make_f_info) would be updated when making $(easy_make_target).
-.PHONY:all
-all:$(easy_make_target)
-	@echo "BUILD_ROOT/TARGET: $(easy_make_target)"
-	@echo "ENTRY: $(call ReadLine,$(easy_make_f_info),1)"
+easy_make_f_target_last_entry:=$(BUILD_ROOT)/easy_make_target_last_entry
 
 
-# clean all .o .d .a .so files and the target recursively in the BUILD_ROOT
-.PHONY: clean
-clean:
-	rm -f $$(find $(BUILD_ROOT) -name *.o)
-	rm -f $$(find $(BUILD_ROOT) -name *.d)
-	rm -f $$(find $(BUILD_ROOT) -name *.a)
-	rm -f $$(find $(BUILD_ROOT) -name *.so)
-	rm -f $$(find $(BUILD_ROOT) -name *.out)
-	rm -f $(easy_make_target)
+# the first default goal
+$(BUILD_ROOT)/target:
 
-.PHONY: help show
-help:show
+
+##
 # show variables in this make file
+#
+.PHONY: show
 show:
 	@echo "---------------------"
 	@echo "basic settings:"
-	@echo "SETTINGS_ROOT       : $(SETTINGS_ROOT)"
 	@echo "BUILD_ROOT          : $(BUILD_ROOT)"
 	@echo "TARGET              : $(TARGET)"
 	@echo "VPATH               : $(VPATH)"
@@ -220,41 +186,27 @@ show:
 	@echo "GXX                 : $(GXX)"
 	@echo "LINKER              : $(LINKER)"
 	@echo "---------------------"
-	@echo "user settings files:"
-	@echo "$(SETTINGS_ROOT)/entry_list"
-	@echo "$(SETTINGS_ROOT)/compile_flags"
-	@echo "$(SETTINGS_ROOT)/compile_search_path"
-	@echo "$(SETTINGS_ROOT)/link_flags"
-	@echo "$(SETTINGS_ROOT)/link_search_path"
-	@echo "---------------------"
 	@echo "user settings:"
 	@echo "ENTRY_LIST          : $(ENTRY_LIST)"
 	@echo "ENTRY               : $(ENTRY)"
-	@echo "COMPILE_FLAGS       : $(COMPILE_FLAGS)"
-	@echo "COMPILE_SEARCH_PATH : $(COMPILE_SEARCH_PATH)"
 	@echo "LINK_FLAGS          : $(LINK_FLAGS)"
-	@echo "LINK_SEARCH_PATH    : $(LINK_SEARCH_PATH)"
+	@echo "AR_FLAGS            : $(AR_FLAGS)"
 	@echo "CPPSOURCES          : $(CPPSOURCES)"
 	@echo "CSOURCES            : $(CSOURCES)"
-	@echo "---------------------"
-	@echo "internal informations:"
-	@echo "easy_make_target           : $(easy_make_target)"
-	@echo "easy_make_all_cppobjects   : $(easy_make_all_cppobjects)"
-	@echo "easy_make_all_cobjects     : $(easy_make_all_cobjects)"
-	@echo "----------"
-	@echo "The following variable is valid after make"
-	@echo "newest entry index           : $(call GetNewestFileIndex,$(easy_make_entry_list))"
-	@echo "newest entry                 : $(word $(call GetNewestFileIndex,$(easy_make_entry_list)),$(easy_make_entry_list))"
-	@echo "files for entry timestamp    : $(foreach file,$(easy_make_entry_list),$(call GetEntryPath4Timestamp,$(file)) )"
-	@echo "  easy_make_entry_list       : $(easy_make_entry_list)"
-	@echo "  easy_make_entry            : $(easy_make_entry)"
-	@echo "  easy_make_entry_link_flags : $(easy_make_entry_link_flags)"
-	@echo "  easy_make_cppsources       : $(easy_make_cppsources)"
-	@echo "  easy_make_csources         : $(easy_make_csources)"
-	@echo "  easy_make_objects          : $(easy_make_objects)"
-	@echo "  easy_make_entry_link_flags : $(easy_make_entry_link_flags)"
 
 
+##
+# clean all .o .d .a .so files recursively in the BUILD_ROOT
+#
+.PHONY: clean
+easy_make_clean:
+	rm -f $$(find $(BUILD_ROOT ) -name "*.o"   )
+	rm -f $$(find $(BUILD_ROOT ) -name "*.d"   )
+	rm -f $$(find $(BUILD_ROOT ) -name "*.a"   )
+	rm -f $$(find $(BUILD_ROOT ) -name "*.so"  )
+	rm -f $$(find $(BUILD_ROOT ) -name "*.out" )
+
+##
 # Pattern rule Descriptions:
 # 1. Prepare the directories, where the object file is gonna be created.
 # 2. Generate the .d dependency file, which specify what files this object 
@@ -268,10 +220,11 @@ show:
 #    that the file $(easy_make_f_detected_entries) is not empty.
 # 7. If there is a main function defined in this object, add this file into the 
 #    list defined in the file $(easy_make_f_detected_entries).
+#
 $(BUILD_ROOT)/%.o: %.$(CPPEXT)
 	@mkdir -p $(dir $@)
-	@$(GXX) -MM -MP -MF"$(@:.o=.d)" -MT"$@" $(COMPILE_FLAGS) $(addprefix -I,$(COMPILE_SEARCH_PATH))  $(word 1,$^) 
-	$(GXX) -c -o $@ $(word 1,$^) $(COMPILE_FLAGS) $(addprefix -I,$(COMPILE_SEARCH_PATH))
+	@$(GXX) -MM -MP -MF"$(@:.o=.d)" -MT"$@" $(COMPILE_FLAGS) $(word 1,$^) 
+	$(GXX) -c -o $@ $(word 1,$^) $(COMPILE_FLAGS)
 	@if [ ! -f $(easy_make_f_detected_entries) ]; then echo " " > $(easy_make_f_detected_entries); fi;
 	@grep -v "^$(patsubst $(BUILD_ROOT)/%.o,%.$(CPPEXT),$@)$$" $(easy_make_f_detected_entries) > $(BUILD_ROOT)/easy_make_entries_tmp.d 
 	@cp $(BUILD_ROOT)/easy_make_entries_tmp.d $(easy_make_f_detected_entries)
@@ -279,15 +232,28 @@ $(BUILD_ROOT)/%.o: %.$(CPPEXT)
 
 $(BUILD_ROOT)/%.o: %.$(CEXT)
 	@mkdir -p $(dir $@)
-	@$(GCC) -MM -MP -MF"$(@:.o=.d)" -MT"$@" $(COMPILE_FLAGS) $(addprefix -I,$(COMPILE_SEARCH_PATH))  $(word 1,$^) 
-	$(GCC) -c -o $@ $(word 1,$^) $(COMPILE_FLAGS) $(addprefix -I,$(COMPILE_SEARCH_PATH))
+	@$(GCC) -MM -MP -MF"$(@:.o=.d)" -MT"$@" $(COMPILE_FLAGS) $(word 1,$^) 
+	$(GCC) -c -o $@ $(word 1,$^) $(COMPILE_FLAGS)
 	@if [ ! -f $(easy_make_f_detected_entries) ]; then echo " " > $(easy_make_f_detected_entries); fi;
 	@grep -v "^$(patsubst $(BUILD_ROOT)/%.o,%.$(CEXT),$@)$$" $(easy_make_f_detected_entries) > $(BUILD_ROOT)/easy_make_entries_tmp.d 
 	@cp $(BUILD_ROOT)/easy_make_entries_tmp.d $(easy_make_f_detected_entries)
 	@if [ $$(nm -g -C --format="posix" $@ | grep -c "^main T") -eq 1 ]; then echo "$(patsubst $(BUILD_ROOT)/%.o,%.$(CEXT),$@)" >> $(easy_make_f_detected_entries) && echo "    entry detected"; fi;
 
 
+##
+# 
+#
+easy_make_build_goals:=$(filter $(BUILD_ROOT)/%,$(MAKECMDGOALS))
+ifeq ($(MAKECMDGOALS),)
+easy_make_build_goals:=$(filter $(BUILD_ROOT)/%,$(.DEFAULT_GOAL))
+endif
+
+easy_make_build_goals:=$(filter-out $(BUILD_ROOT)/%.o $(BUILD_ROOT)/%.d $(BUILD_ROOT)/easy_make%,$(easy_make_build_goals))
+ifneq ($(easy_make_build_goals),)
+
+##
 # include all generated dependency files
+#
 ifneq ($(strip $(easy_make_all_cppobjects)),)
     sinclude $(easy_make_all_cppobjects:.o=.d)
 endif
@@ -295,25 +261,48 @@ ifneq ($(strip $(easy_make_all_cobjects)),)
     sinclude $(easy_make_all_cobjects:.o=.d)
 endif
 
-.PHONY: easy_make_phony
-# do nothing to make this target.
-easy_make_phony:
-	@true
+
+##
+# If ENTRY is explicitly specified from command line, then check if 
+# this ENTRY is different from last last the ENTRY last built with.
+# If different, make the goals phony targets.
+# @note Use the function "filter" instead of "findstring" here, because
+#   $(findstring a,abc) will get an "a". we need to use "filter" to 
+#   compare the equality of a and abc.
+#
+ifneq ($(ENTRY),)
+.PHONY: $(foreach goal,$(easy_make_build_goals),$(if $(filter $(ENTRY),$(call ConfReadValue,$(easy_make_f_target_last_entry),$(goal))),,$(goal)))
+endif
 
 
-easy_make_entry_list=$(ENTRY_LIST) $(call ReadSettings,$(easy_make_f_detected_entries))
-easy_make_entry=$(call GetEntry,$(ENTRY),$(easy_make_entry_list),"ENTRY=$(ENTRY) is neither defined in the entry_list nor detected by easy_make.")
+##
 # easy_make_cppsources: cpp files with unused entries filtered-out.
-easy_make_cppsources=$(call SourcesToLink,$(CPPSOURCES),$(easy_make_entry),$(easy_make_entry_list))
-easy_make_csources=$(call SourcesToLink,$(CSOURCES),$(easy_make_entry),$(easy_make_entry_list))
-easy_make_objects=$(call CorrendingObjects,$(easy_make_cppsources),$(BUILD_ROOT),$(CPPEXT)) $(call CorrendingObjects,$(easy_make_csources),$(BUILD_ROOT),$(CEXT))
-easy_make_entry_link_flags=$(call ReadSettings,$(SETTINGS_ROOT)/$(easy_make_entry).link_flags) $(ENTRY_$(strip $(easy_make_entry))_LINK_FLAGS)
-# The $(if ...) block makes sure that the $(easy_make_target) is re-created
-# when different $(TARGET) or different $(ENTRY) is set by user at command line
-$(easy_make_target): $(easy_make_all_cppobjects) $(easy_make_all_cobjects) $(if $(filter-out $(prev_entry) $(prev_target),$(if $(ENTRY),$(ENTRY),$(prev_entry)) $(TARGET)),easy_make_phony,)
-	@echo ""
-	@echo "[NOTICE]: LINKING USING **$(easy_make_entry)** AS ENTRY"
-	$(if $(call ENTRY_$(strip $(easy_make_entry))_LinkCmd,$@,$(easy_make_objects)),@ true || ,)$(LINKER) -o $@ $(easy_make_objects) $(LINK_FLAGS) $(easy_make_entry_link_flags) $(addprefix -L,$(LINK_SEARCH_PATH))
-	$(call ENTRY_$(strip $(easy_make_entry))_LinkCmd,$@,$(easy_make_objects))
-	@echo "$(easy_make_entry)" > $(easy_make_f_info)
-	@echo "$(TARGET)" >> $(easy_make_f_info)
+#
+easy_make_entry_list = $(ENTRY_LIST) $(call ReadSettings,$(easy_make_f_detected_entries))
+easy_make_entry      = $(if $(filter $(ENTRY),NONE)$(filter $(ENTRY),none),,$(call GetEntry,$(ENTRY),$(easy_make_entry_list),"ENTRY=$(ENTRY) is neither defined in the entry_list nor detected by easy_make."))
+easy_make_cppsources = $(call FilterSourcesToLink , $(CPPSOURCES) , $(easy_make_entry) , $(easy_make_entry_list))
+easy_make_csources   = $(call FilterSourcesToLink , $(CSOURCES)   , $(easy_make_entry) , $(easy_make_entry_list))
+easy_make_objects    = $(call GetCorrendingObjects,$(easy_make_cppsources),$(BUILD_ROOT),$(CPPEXT)) $(call GetCorrendingObjects,$(easy_make_csources),$(BUILD_ROOT),$(CEXT))
+
+easy_make_build_goals_ar   := $(filter %.a,$(easy_make_build_goals))
+easy_make_build_goals      := $(filter-out %.a,$(easy_make_build_goals))
+easy_make_build_goals_link := $(easy_make_build_goals)
+
+##
+# note: When different $(TARGET) or different $(ENTRY) is set by user at command line, this build would be PHONY
+#
+$(easy_make_build_goals_link): $(easy_make_all_cppobjects) $(easy_make_all_cobjects)
+	@echo
+	$(LINKER) -o $@ $(easy_make_objects) $(LINK_FLAGS)
+	@echo "ENTRY  :      $(easy_make_entry)"
+	@echo "TARGET :      $@"
+	@$(call CmdConfWriteValue,$(easy_make_f_target_last_entry),$@,$(easy_make_entry))
+
+$(easy_make_build_goals_ar): $(easy_make_all_cppobjects) $(easy_make_all_cobjects)
+	@echo
+	$(AR) cr $@ $(easy_make_objects) $(AR_FLAGS)
+	@echo "ENTRY  :      $(easy_make_entry)"
+	@echo "TARGET :      $@"
+	@$(call CmdConfWriteValue,$(easy_make_f_target_last_entry),$@,$(easy_make_entry))
+
+endif
